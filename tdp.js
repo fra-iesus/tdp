e9166c2bdf09717d53e312bf4594d9ce53d37f1f
Alexej Sidorenko <fra.iesus@gmail.com>
2016-07-11 15:39:45 +0200

    Update tdp.js

--- tdp.js
+++ tdp.js
@@ -448,7 +448,7 @@ https://github.com/fra-iesus/tdp
 									option.attr("disabled");
 								}
 								if (input.value == setting[1]) {
-									option.attr("selected", "selected");
+									option.attr("selected");
 								}
 								input_element.append(option);
 							});
